ee3884ea39c11b0d0f8441e74663d5c09431a3f2
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2025-09-15 16:30:01 +0000

    Display 5 random images on every render

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -1,8 +1,25 @@
-export default async function getPokemons() {
-  try {
-    const respose = await fetch(`https://pokeapi.co/api/v2/pokemon-form/7/`);
-    return await respose.json();
-  } catch (err) {
-    console.log(err);
+// from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+}
+
+export default async function getPokemons(n = 5) {
+  const responses = [];
+  const set = new Set();
+  while (responses.length != n) {
+    const id = getRandomInt(1, 152);
+    if (set.has(id)) continue;
+    try {
+      const respose = await fetch(
+        `https://pokeapi.co/api/v2/pokemon-form/${id}/`,
+      );
+      responses.push(await respose.json());
+    } catch (err) {
+      console.log(err);
+    }
   }
+
+  return responses;
 }
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,6 +9,8 @@ function App() {
   const [jsonData, setJsonData] = useState(null);
   const [loading, setLoading] = useState(true);
 
+  const numPokes = 5;
+
   useEffect(() => {
     (async function getData() {
       const data = await getPokemons();
@@ -18,14 +20,27 @@ function App() {
     })();
   }, []);
 
-  if (loading) return <div className="pokemon">Loading</div>;
+  if (loading)
+    return Array.from({ length: numPokes }).map((_, index) => (
+      <div key={index} className="pokemon">
+        Loading
+      </div>
+    ));
 
-  return (
-    <div className="pokemon">
-      <img src={jsonData.sprites.front_default} alt="pokemon-sprite" />
-      <div className="title">{jsonData.name}</div>
+  return jsonData.map((data, index) => (
+    <div className="pokemon" key={index}>
+      <img
+        className="sprite"
+        src={data.sprites.front_default}
+        alt="pokemon-sprite"
+        height="100"
+        width="100"
+      />
+      <div className="title">
+        {data.name.slice(0, 1).toUpperCase() + data.name.slice(1)}
+      </div>
     </div>
-  );
+  ));
 }
 
 export default App;
