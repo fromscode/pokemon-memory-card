8a1be7293477ae956196b840a57c883d1d68af98
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2025-10-08 05:00:21 +0000

    Create Game component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,53 +1,10 @@
 import "../styles/reset.css";
 import "../styles/App.css";
 
-import "../api/pokemon.js";
-import getPokemons from "../api/pokemon.js";
-import randomizer from "../helpers/randomizer.js";
-
-import { useEffect, useState } from "react";
+import Game from "./Game";
 
 function App() {
-  const [jsonData, setJsonData] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  const numPokes = 5;
-
-  useEffect(() => {
-    (async function getData() {
-      const data = await getPokemons();
-      console.log(data);
-      setJsonData(data);
-      setLoading(false);
-    })();
-  }, []);
-
-  if (loading)
-    return Array.from({ length: numPokes }).map((_, index) => (
-      <div key={index} className="pokemon">
-        Loading
-      </div>
-    ));
-
-  return jsonData.map((data, index) => (
-    <div
-      className="pokemon"
-      key={index}
-      onClick={() => {
-        const randomizedJsonData = randomizer(jsonData);
-        setJsonData(randomizedJsonData);
-      }}
-    >
-      <img
-        className="sprite"
-        src={data.sprite}
-        alt="pokemon-sprite"
-        height="100"
-        width="100"
-      />
-      <div className="title">{data.name.toUpperCase()}</div>
-    </div>
-  ));
+  return <Game />;
 }
 
 export default App;
