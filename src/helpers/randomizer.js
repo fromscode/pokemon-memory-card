50dbf9e02d887d58f9c4ca45955785bad79ce2a3
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2025-10-15 06:37:56 +0000

    Fix randomizer logic to prevent infinite loop

--- src/helpers/randomizer.js
+++ src/helpers/randomizer.js
@@ -1,22 +1,24 @@
-// from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-function getRandomInt(min, max) {
-  const minCeiled = Math.ceil(min);
-  const maxFloored = Math.floor(max);
-  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
-}
-
+// Fisher–Yates (aka Knuth) Shuffle.
+// from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 export default function randomizer(arr) {
-  const newArr = new Array(arr.length);
-  const set = new Set();
-
-  arr.forEach((value, index) => {
-    let newIndex = getRandomInt(0, arr.length);
-    while (set.has(newIndex) || newIndex == index) {
-      newIndex = getRandomInt(0, arr.length);
-    }
-    newArr[newIndex] = { ...value };
-    set.add(newIndex);
+  const newArr = arr.map((value) => {
+    return { ...value };
   });
+
+  let currentIndex = newArr.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [newArr[currentIndex], newArr[randomIndex]] = [
+      newArr[randomIndex],
+      newArr[currentIndex],
+    ];
+  }
 
   return newArr;
 }
